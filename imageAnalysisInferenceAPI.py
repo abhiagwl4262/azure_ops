29a8f607bfe26c07a7f6a3f31ce5eb626aee1aeb
abhishek.agrawal <abhishek.agrawal@eagleview.com>
2024-04-03 13:36:16 +0530

    plotting of imageAnalysisInferenceAPI added

--- imageAnalysisInferenceAPI.py
+++ imageAnalysisInferenceAPI.py
@@ -1,12 +1,50 @@
 import http.client, urllib.request, urllib.parse, urllib.error, base64
 import os, json
+from PIL import Image
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+
+def plot(results, img_path):
+    # Load image from URL
+    image = Image.open(img_path)
+    
+    # Plot the image
+    plt.figure(figsize=(10, 10))
+    plt.imshow(image)
+    
+    # Get image dimensions
+    width, height = image.size
+    
+    # Plot each prediction as a bounding box
+    for prediction in results:
+        tag = prediction['tags'][0]['name']
+        probability = prediction['tags'][0]['confidence']
+        bbox = prediction['boundingBox']
+        
+        # Convert bounding box coordinates from relative to absolute
+        left = bbox['x'] * width
+        top = bbox['y'] * height
+        right = left + bbox['w'] * width
+        bottom = top + bbox['h'] * height
+        
+        # Create a rectangle patch
+        rect = patches.Rectangle((left, top), right - left, bottom - top, linewidth=1, edgecolor='r', facecolor='none')
+        
+        # Add the patch to the plot
+        plt.gca().add_patch(rect)
+        
+        # Add text label
+        plt.text(left, top - 5, f'{tag} ({probability:.2f})', color='r', fontsize=10, backgroundcolor='w')
+    
+    plt.axis('off')
+    plt.show()
+
 
 headers = {
     # Request headers
     "Content-Type": "application/octet-stream",
     "Ocp-Apim-Subscription-Key": os.environ.get("OCP_APIM_SUBSCRIPTION_KEY")
 }
-
 
 # The path to your image file
 image_path = "valid/Abee1_12_png.rf.3415fd50c0de26c1ad6e3c2fc391f47a.jpg"
@@ -22,6 +60,7 @@ params = urllib.parse.urlencode(
     }
 )
 
+conf_threshold = 0.5
 try:
     conn = http.client.HTTPSConnection(
         "object-detection-v2.cognitiveservices.azure.com"
@@ -38,9 +77,14 @@ try:
 
     json_str = data.decode("utf-8")
     json_objs = json.loads(json_str)
+    detections = []
+    for detection in json_objs["customModelResult"]["objectsResult"]["values"]:
+        if detection["tags"][0]["confidence"] > conf_threshold:
+            detections.append(detection)
+    print(detections)
+    plot(detections, image_path)
     with open("response.json", "w") as fw:
         fw.write(json.dumps(json_objs, indent=4))
-    print(json.dumps(json_objs, indent=4))
     conn.close()
 except Exception as e:
     print("[Errno {0}] {1}".format(e.errno, e.strerror))
