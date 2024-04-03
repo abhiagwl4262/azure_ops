3082424d0ebcbbd6ba4c5f84de809c02693bf11a
Rishab Pal <palrishab569@gmail.com>
2024-04-03 16:53:12 +0530

    modified stuff

--- customvision_inferenceAPI.py
+++ customvision_inferenceAPI.py
@@ -5,67 +5,95 @@ from PIL import Image
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 
+
+os.environ["VISION_PREDICTION_KEY"] = "5deb696fc78948469145457b4d52fe11"
+
 def plot(results, img_path):
     # Load image from URL
     image = Image.open(img_path)
-    
+
     # Plot the image
     plt.figure(figsize=(10, 10))
     plt.imshow(image)
-    
+
     # Get image dimensions
     width, height = image.size
-    
+
     # Plot each prediction as a bounding box
     for prediction in results:
-        tag = prediction['tagName']
-        probability = prediction['probability']
-        bbox = prediction['boundingBox']
-        
+        tag = prediction["tagName"]
+        probability = prediction["probability"]
+        bbox = prediction["boundingBox"]
+
         # Convert bounding box coordinates from relative to absolute
-        left = bbox['left'] * width
-        top = bbox['top'] * height
-        right = left + bbox['width'] * width
-        bottom = top + bbox['height'] * height
-        
+        left = bbox["left"] * width
+        top = bbox["top"] * height
+        right = left + bbox["width"] * width
+        bottom = top + bbox["height"] * height
+
         # Create a rectangle patch
-        rect = patches.Rectangle((left, top), right - left, bottom - top, linewidth=1, edgecolor='r', facecolor='none')
-        
+        rect = patches.Rectangle(
+            (left, top),
+            right - left,
+            bottom - top,
+            linewidth=1,
+            edgecolor="r",
+            facecolor="none",
+        )
+
         # Add the patch to the plot
         plt.gca().add_patch(rect)
-        
+
         # Add text label
-        plt.text(left, top - 5, f'{tag} ({probability:.2f})', color='r', fontsize=10, backgroundcolor='w')
-    
-    plt.axis('off')
+        plt.text(
+            left,
+            top - 5,
+            f"{tag} ({probability:.2f})",
+            color="r",
+            fontsize=10,
+            backgroundcolor="w",
+        )
+
+    plt.axis("off")
     plt.show()
 
-prediction_url = "https://customobjectdetection-prediction.cognitiveservices.azure.com/customvision/v3.0/Prediction/8f29a6c3-5b39-4999-92b6-e5d9f3573ec9/detect/iterations/Iteration%204/image"
 
-headers = {
-    # Request headers
-    "Content-Type": "application/octet-stream",
-    "Prediction-Key": os.environ.get("VISION_PREDICTION_KEY")
-}
-# The path to your image file
-image_path = "valid/Abee1_12_png.rf.3415fd50c0de26c1ad6e3c2fc391f47a.jpg"
-# Read the image file in binary mode
-with open(image_path, "rb") as image_file:
-    data = image_file.read()
+def infer(image_path, conf_threshold=0.5):
+    prediction_url = "https://customobjectdetection-prediction.cognitiveservices.azure.com/customvision/v3.0/Prediction/8f29a6c3-5b39-4999-92b6-e5d9f3573ec9/detect/iterations/Iteration%204/image"
 
-conf_threshold=0.5
-try:
-    response = requests.post(prediction_url, data=data, headers=headers)
-    if response.status_code == 200:
-        # Parse and print the prediction results
-        results = json.loads(response.content.decode('utf-8'))
-        filtered_preds = []
-        for i, prediction in enumerate(results['predictions']):
-            if prediction['probability'] >= conf_threshold:
-                filtered_preds.append(prediction)
-            print(f"Tag: {prediction['tagName']}, Probability: {prediction['probability']}")        
-        plot(filtered_preds, image_path)
-    else:
-        print(f"Prediction failed with status code {response.status_code}: {response.text}")
-except Exception as e:
-    print("[Errno {0}] {1}".format(e.errno, e.strerror))
+    headers = {
+        # Request headers
+        "Content-Type": "application/octet-stream",
+        "Prediction-Key": os.environ.get("VISION_PREDICTION_KEY"),
+    }
+
+    # Read the image file in binary mode
+    with open(image_path, "rb") as image_file:
+        data = image_file.read()
+
+    try:
+        response = requests.post(prediction_url, data=data, headers=headers)
+        if response.status_code == 200:
+            # Parse and print the prediction results
+            results = json.loads(response.content.decode("utf-8"))
+            filtered_preds = []
+            for i, prediction in enumerate(results["predictions"]):
+                if prediction["probability"] >= conf_threshold:
+                    filtered_preds.append(prediction)
+                    print(
+                        f"Tag: {prediction['tagName']}, Probability: {prediction['probability']}"
+                    )
+            # plot(filtered_preds, image_path)
+        else:
+            print(
+                f"Prediction failed with status code {response.status_code}: {response.text}"
+            )
+    except Exception as e:
+        print("[Errno {0}] {1}".format(e.errno, e.strerror))
+
+
+if __name__ == "__main__":
+    conf_threshold = 0.5
+    image_path = "Image 2024-03-26 at 09.56.02.jpeg"
+
+    infer(image_path, conf_threshold)
--- customvision_inferenceSDK.py
+++ customvision_inferenceSDK.py
@@ -1,18 +1,33 @@
-from azure.cognitiveservices.vision.customvision.training import CustomVisionTrainingClient
-from azure.cognitiveservices.vision.customvision.prediction import CustomVisionPredictionClient
-from azure.cognitiveservices.vision.customvision.training.models import ImageFileCreateBatch, ImageFileCreateEntry, Region
+from azure.cognitiveservices.vision.customvision.training import (
+    CustomVisionTrainingClient,
+)
+from azure.cognitiveservices.vision.customvision.prediction import (
+    CustomVisionPredictionClient,
+)
+from azure.cognitiveservices.vision.customvision.training.models import (
+    ImageFileCreateBatch,
+    ImageFileCreateEntry,
+    Region,
+)
 from msrest.authentication import ApiKeyCredentials
 import os, json, time
 
-def add_data(project, tags, image_dir, ann_path):
 
-    print ("Adding images...")
+VISION_TRAINING_ENDPOINT = os.environ.get("VISION_TRAINING_ENDPOINT")
+TRAINING_KEY = os.environ.get("VISION_TRAINING_KEY")
+VISION_PREDICTION_ENDPOINT = os.environ.get("VISION_PREDICTION_ENDPOINT")
+PREDICTION_KEY = os.environ.get("VISION_PREDICTION_KEY")
+PROJECT_ID = os.environ.get("PROJECT_ID")
+
+
+def add_data(project, tags, image_dir, ann_path):
+    print("Adding images...")
     tag_dict = {}
     for tag in tags:
         tag_dict[tag.name] = tag
 
     tagged_images_with_regions = []
-    f =open(ann_path)
+    f = open(ann_path)
     data = json.load(f)
 
     category_id_name_dict = {}
@@ -24,74 +39,107 @@ def add_data(project, tags, image_dir, ann_path):
         img_id_fname_dict[img["id"]] = img["file_name"]
 
     for ann in data["annotations"]:
-        x,y,w,h = ann["bbox"]
+        x, y, w, h = ann["bbox"]
         tag_id = tag_dict[category_id_name_dict[ann["category_id"]]].id
-        regions = [Region(tag_id=tag_id, left=x,top=y,width=w,height=h)]
+        regions = [Region(tag_id=tag_id, left=x, top=y, width=w, height=h)]
         file_name = img_id_fname_dict[ann["image_id"]]
         with open(os.path.join(image_dir, file_name), mode="rb") as image_contents:
-            tagged_images_with_regions.append(ImageFileCreateEntry(name=file_name, contents=image_contents.read(), regions=regions))
+            tagged_images_with_regions.append(
+                ImageFileCreateEntry(
+                    name=file_name, contents=image_contents.read(), regions=regions
+                )
+            )
 
     # upload_result = trainer.create_images_from_files(project.id, ImageFileCreateBatch(images=tagged_images_with_regions))
-    for i in range(0,len(tagged_images_with_regions),64):
-        batch = ImageFileCreateBatch(images=tagged_images_with_regions[i:i+64])
+    for i in range(0, len(tagged_images_with_regions), 64):
+        batch = ImageFileCreateBatch(images=tagged_images_with_regions[i : i + 64])
         upload_result = trainer.create_images_from_files(project.id, batch)
 
-        if not upload_result.is_batch_successful: 
+        if not upload_result.is_batch_successful:
             print("Image batch upload failed.")
             for image in upload_result.images:
                 print("Image status: ", image.status)
-    return
-#training creds
-training_key = os.environ.get("VISION_TRAINING_KEY")
-VISION_TRAINING_ENDPOINT = os.environ.get("VISION_TRAINING_ENDPOINT")
-credentials = ApiKeyCredentials(in_headers={"Training-key": training_key})
-trainer = CustomVisionTrainingClient(VISION_TRAINING_ENDPOINT, credentials)
-
-# Get existing project
-project = trainer.get_project(project_id=os.environ.get("PROJECT_ID"))
-
-# fetch tags
-tags = trainer.get_tags(project.id)
-if not len(tags):
-    car_tag = trainer.create_tag(project.id, "car")
-    person_tag = trainer.create_tag(project.id, "person")
-    tags = [car_tag, person_tag]
-
-add_data(project, tags, "train", "train_new.json")
-# add_data(project, tags, "valid", "valid_new.json")
-
-##Train
-print ("Training...")
-# iteration = trainer.train_project(project.id) #creates new iteration
-iteration = trainer.get_iterations(project.id)[0] #Use last iteration
-while (iteration.status != "Completed"):
-    iteration = trainer.get_iteration(project.id, iteration.id)
-    print ("Training status: " + iteration.status)
-    print ("Waiting 10 seconds...")
-    time.sleep(10)
-
-prediction_resource_id=os.environ.get("VISION_PREDICTION_RESOURCE_ID")
-
-#The iteration is now trained. Publish it to the project endpoint
-try:
-    trainer.publish_iteration(project.id, iteration.id, iteration.name, prediction_resource_id)
-    print ("Done!")
-except Exception as e:
-    print(e)
-
-#prediction creds
-VISION_PREDICTION_ENDPOINT = os.environ.get("VISION_PREDICTION_ENDPOINT")
-prediction_key=os.environ.get("VISION_PREDICTION_KEY")
-prediction_credentials = ApiKeyCredentials(in_headers={"Prediction-key": prediction_key})
-predictor = CustomVisionPredictionClient(VISION_PREDICTION_ENDPOINT, prediction_credentials)
 
 
-#inference
-with open("valid/Abee1_12_png.rf.3415fd50c0de26c1ad6e3c2fc391f47a.jpg", mode="rb") as test_data:
-    results = predictor.detect_image(project.id, iteration.name, test_data)
+def trainer():
+    # training creds
+    credentials = ApiKeyCredentials(in_headers={"Training-key": TRAINING_KEY})
+    trainer = CustomVisionTrainingClient(VISION_TRAINING_ENDPOINT, credentials)
 
-breakpoint()
-# Display the results.
-for prediction in results.predictions:
-    if prediction.probability > 0.5:
-        print("\t" + prediction.tag_name + ": {0:.2f}% bbox.left = {1:.2f}, bbox.top = {2:.2f}, bbox.width = {3:.2f}, bbox.height = {4:.2f}".format(prediction.probability * 100, prediction.bounding_box.left, prediction.bounding_box.top, prediction.bounding_box.width, prediction.bounding_box.height))
+    # Get existing project
+    project = trainer.get_project(project_id=PROJECT_ID)
+
+    # fetch tags
+    tags = trainer.get_tags(project.id)
+    if not len(tags):
+        car_tag = trainer.create_tag(project.id, "car")
+        person_tag = trainer.create_tag(project.id, "person")
+        tags = [car_tag, person_tag]
+
+    add_data(project, tags, "train", "train_new.json")
+    # add_data(project, tags, "valid", "valid_new.json")
+
+    ##Train
+    print("Training...")
+    # iteration = trainer.train_project(project.id) #creates new iteration
+    iteration = trainer.get_iterations(project.id)[0]  # Use last iteration
+    while iteration.status != "Completed":
+        iteration = trainer.get_iteration(project.id, iteration.id)
+        print("Training status: " + iteration.status)
+        print("Waiting 10 seconds...")
+        time.sleep(10)
+
+    prediction_resource_id = os.environ.get("VISION_PREDICTION_RESOURCE_ID")
+    iteration = trainer.get_iterations(project.id)[0]  # Use last iteration
+
+    # The iteration is now trained. Publish it to the project endpoint
+    try:
+        trainer.publish_iteration(
+            project.id, iteration.id, iteration.name, prediction_resource_id
+        )
+        print("Done!")
+    except Exception as e:
+        print(e)
+
+
+def infer(image_path, conf_threshold=0.5):
+    if not os.path.exists(image_path):
+        raise FileNotFoundError(f"{image_path} does not exist!")
+
+    credentials = ApiKeyCredentials(in_headers={"Training-key": TRAINING_KEY})
+    trainer = CustomVisionTrainingClient(VISION_TRAINING_ENDPOINT, credentials)
+
+    project = trainer.get_project(project_id=os.environ.get("PROJECT_ID"))
+    iteration = trainer.get_iterations(project.id)[0]  # Use last iteration
+    # prediction creds
+    prediction_credentials = ApiKeyCredentials(
+        in_headers={"Prediction-key": PREDICTION_KEY}
+    )
+    predictor = CustomVisionPredictionClient(
+        VISION_PREDICTION_ENDPOINT, prediction_credentials
+    )
+    # inference
+    with open(image_path, mode="rb") as test_data:
+        results = predictor.detect_image(project.id, iteration.name, test_data)
+
+    # Display the results.
+    for prediction in results.predictions:
+        if prediction.probability >= conf_threshold:
+            print(
+                "\t"
+                + prediction.tag_name
+                + ": {0:.2f}% bbox.left = {1:.2f}, bbox.top = {2:.2f}, bbox.width = {3:.2f}, bbox.height = {4:.2f}".format(
+                    prediction.probability * 100,
+                    prediction.bounding_box.left,
+                    prediction.bounding_box.top,
+                    prediction.bounding_box.width,
+                    prediction.bounding_box.height,
+                )
+            )
+
+
+if __name__ == "__main__":
+    conf_threshold = 0.5
+    image_path = "Image 2024-03-26 at 09.56.02.jpeg"
+
+    infer(image_path, conf_threshold)
