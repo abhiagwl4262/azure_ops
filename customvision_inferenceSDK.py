c3e242cbd55d43e9c7fa730b52d6c6e51a31e8cd
abhishek.agrawal <abhishek.agrawal@eagleview.com>
2024-04-05 20:49:12 +0530

    bg data added

--- customvision_inferenceSDK.py
+++ customvision_inferenceSDK.py
@@ -19,6 +19,38 @@ VISION_PREDICTION_ENDPOINT = os.environ.get("VISION_PREDICTION_ENDPOINT")
 PREDICTION_KEY = os.environ.get("VISION_PREDICTION_KEY")
 PROJECT_ID = os.environ.get("PROJECT_ID")
 
+def add_BG_data(trainer, project, tags, image_dir, pred_dir):
+    print("Adding images...")
+    tag_dict = {}
+    for tag in tags:
+        tag_dict[tag.name] = tag
+    tagged_images_with_regions = []
+
+    for fname in os.listdir(image_dir):
+        fname_txt = fname.replace(".png", ".txt")
+        if fname_txt in os.listdir(pred_dir):
+            ann_path = os.path.join(pred_dir, fname_txt)            
+            lines = open(ann_path).readlines()
+            for line in lines:
+                parts = line.strip("\n").split(",")
+                bbox = [float(x) for x in parts[-4:]]
+                x, y, w, h = bbox                
+                regions = [Region(tag_id=tag_dict["bg"].id, left=x, top=y, width=w, height=h)]
+                with open(os.path.join(image_dir, fname), mode="rb") as image_contents:
+                    tagged_images_with_regions.append(
+                        ImageFileCreateEntry(
+                            name=fname, contents=image_contents.read(), regions=regions
+                        )
+                    )
+    # upload_result = trainer.create_images_from_files(project.id, ImageFileCreateBatch(images=tagged_images_with_regions))
+    for i in range(0, len(tagged_images_with_regions), 64):
+        batch = ImageFileCreateBatch(images=tagged_images_with_regions[i : i + 64])
+        upload_result = trainer.create_images_from_files(project.id, batch)
+
+        if not upload_result.is_batch_successful:
+            print("Image batch upload failed.")
+            for image in upload_result.images:
+                print("Image status: ", image.status)
 
 def add_data(project, tags, image_dir, ann_path):
     print("Adding images...")
@@ -61,7 +93,7 @@ def add_data(project, tags, image_dir, ann_path):
                 print("Image status: ", image.status)
 
 
-def trainer():
+def training_func():
     # training creds
     credentials = ApiKeyCredentials(in_headers={"Training-key": TRAINING_KEY})
     trainer = CustomVisionTrainingClient(VISION_TRAINING_ENDPOINT, credentials)
@@ -76,10 +108,12 @@ def trainer():
         person_tag = trainer.create_tag(project.id, "person")
         tags = [car_tag, person_tag]
 
-    add_data(project, tags, "train", "train_new.json")
+    # add_data(project, tags, "train", "train_new.json")
     # add_data(project, tags, "valid", "valid_new.json")
-
+    # bg_tag = trainer.create_tag(project.id, "bg")    
+    add_BG_data(trainer, project, tags, "Security_Camera_Clips/FalsePositives/images", "customVisionOutput")
     ##Train
+    return
     print("Training...")
     # iteration = trainer.train_project(project.id) #creates new iteration
     iteration = trainer.get_iterations(project.id)[0]  # Use last iteration
@@ -141,5 +175,5 @@ def infer(image_path, conf_threshold=0.5):
 if __name__ == "__main__":
     conf_threshold = 0.5
     image_path = "Image 2024-03-26 at 09.56.02.jpeg"
-
+    training_func()
     infer(image_path, conf_threshold)
